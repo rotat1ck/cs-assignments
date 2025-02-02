a93168442acdfcf663b7faf9e56be003aba7aab5
rotat1ck <rotatick32151@gmail.com>
2025-02-02 05:34:38 +0300

    mega .sln file, easier cloning

---
+++ homework/2025.01/10.02 homework/Program.cs
@@ -0,0 +1,42 @@
+﻿class Code {
+    static void Main() {
+        Console.Clear();
+        while (true) {
+            Console.Clear();
+            ConsoleKey press = getKeyPress("ESC - Close the program\nEnter the number of a task(0 - 9): ");
+            switch (press) {
+                case ConsoleKey.D0: {
+                    
+                    break;
+                } default: {
+                    invalidInput = true;
+                    break;
+                }
+            }
+        }
+
+        // 🥨
+    }
+    static bool invalidInput = false; // 😃😃😃
+
+    static ConsoleKey getKeyPress(string msg) {
+        ConsoleKeyInfo keyPressed;
+
+        while (true) {
+            if (invalidInput) {
+                Console.WriteLine("Invalid input!\n");
+            }
+            invalidInput = false;
+            Console.Write(msg);
+            keyPressed = Console.ReadKey();
+            Console.WriteLine();
+
+            if (keyPressed.Key == ConsoleKey.Escape) {
+                Environment.Exit(0);
+            } else {
+                return keyPressed.Key;
+            }
+        }
+        
+    }
+}

b8e0932c353ca9faa8192a85a3865211d2f4481e
rotat1ck <rotatick32151@gmail.com>
2025-02-04 15:41:01 +0300

    14.02 homework

--- homework/2025.02/14.02 homework/Program.cs
+++ homework/2025.02/14.02 homework/Program.cs
@@ -2,7 +2,7 @@
     static void Main() {
         Console.Clear();
         while (true) {
-            ConsoleKey press = getKeyPress("ESC - Close the program\nEnter the number of a task(1 - 5): ");
+            ConsoleKey press = getKeyPress("ESC - Close the program\nEnter the number of a task(1 - 6): ");
             Console.Clear();
             switch (press) {
                 case ConsoleKey.D1: {
@@ -19,6 +19,9 @@
                     break;
                 } case ConsoleKey.D5: {
                     Five();
+                    break;
+                } case ConsoleKey.D6: {
+                    Six();
                     break;
                 } default: {
                     invalidInput = true;
@@ -49,6 +52,112 @@
                 return keyPressed.Key;
             }
         }
+    }
+
+    static void One() {
+        double a, b, c;
+        Console.Write("Enter A: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter B: ");
+        b = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter C: ");
+        c = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("True statements: ");
+
+        if (a < b && b < c) {
+            Console.WriteLine($"{a} < {b} < {c}");
+        } else if (b > a && a > c) {
+            Console.WriteLine($"{b} > {a} > {c}");
+        }
+        Console.WriteLine();
+    }
+
+    static void Two() {
+        double a, b, c, d;
+        Console.Write("Enter A: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter B: ");
+        b = Convert.ToDouble(Console.ReadLine());
+        if (a < 0 || b < 0) {
+            return;
+        }
+        Console.Write("Enter C: ");
+        c = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter D: ");
+        d = Convert.ToDouble(Console.ReadLine());
+
+        if (a % b == c || a % b == d) {
+            Console.WriteLine($"{a} % {b} == {c} || {d}");
+        } else {
+            Console.WriteLine($"{a} % {b} != {c} || {d}");
+        }
+        Console.WriteLine();
+    }
+
+    static void Three() {
+        double a, b, c, d;
+        Console.Write("Enter A: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter B: ");
+        b = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter C: ");
+        c = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter D: ");
+        d = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Max: {Math.Max(Math.Max(a, b), Math.Max(c, d))}");
+        Console.WriteLine($"Min: {Math.Min(Math.Min(a, b), Math.Min(c, d))}");
+    }
+
+    static void Four() {
+        double a, b;
+        Console.Write("Enter A: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter B: ");
+        b = Convert.ToDouble(Console.ReadLine());
         
+        a = a < 0 ? -a : a;
+        b = b < 0 ? -b : b;
+
+        double halfSum = (a + b) / 2;
+        double sqrt = Math.Sqrt(a * b);
+
+        Console.WriteLine($"Halfsum: {halfSum}");
+        Console.WriteLine($"Square root of halfsum: {sqrt}");
+        Console.WriteLine();
+    }
+
+    static void Five() {
+        double a, b, c;
+        Console.Write("Enter A: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter B: ");
+        b = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter C: ");
+        c = Convert.ToDouble(Console.ReadLine());
+
+        if (a == b && b == c) {
+            Console.WriteLine("Height is same");
+        } else {
+            Console.WriteLine("Height isn't same");
+        }
+        Console.WriteLine();
+    }
+
+    static void Six() {
+        double weight;
+        Console.Write("Enter weight: ");
+        weight = Convert.ToDouble(Console.ReadLine());
+
+        if (weight < 60) {
+            Console.WriteLine("First group");
+        } else if (weight < 64 ) {
+            Console.WriteLine("Second group");
+        } else if (weight < 69) {
+            Console.WriteLine("Third group");
+        } else {
+            Console.WriteLine("No group");
+        }
+        Console.WriteLine();
     }
 }

5e6c548cb4ff194e02f0ae4c37eeaf8d0a1a4a9f
rotat1ck <rotatick32151@gmail.com>
2025-02-10 12:01:57 +0300

    20.02 lab, no excel table for 2 task

---
+++ homework/2025.02/20.02 lab/func/Program.cs
@@ -0,0 +1,57 @@
+﻿class Func {
+    private int a;
+    private int b;
+    private int c;
+
+    public Func(int a, int b, int c) {
+        if (a == 0) {
+            throw new Exception("A cannot be zero");
+        }
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double FindD() {
+        return b * b - 4 * a * c;
+    }
+
+    public double[] Solve() {
+        double D = b * b - 4 * a * c;
+        if (D < 0) {
+            return [];
+        } else if (D == 0) {
+            double x = -b / (2 * a);
+            return [x];
+        } else {
+            double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+            return [x1, x2];
+        }
+    }
+}
+
+class Code {
+    static void Main() {
+        try {
+            Console.Write("Введите a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите c: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Func func = new Func(a, b, c);
+
+            double[] res = func.Solve();
+            foreach (double i in res) {
+                Console.WriteLine($"Решение: {i}");
+            }
+        } catch (Exception ex) {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
---
+++ homework/2025.02/20.02 lab/triangle/Program.cs
@@ -0,0 +1,73 @@
+﻿class Triangle {
+    private int a;
+    private int b;
+    private int c;
+    private string type = "";
+
+    public Triangle(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new Exception("Стороны треугольника должны быть больше 0");
+        }
+
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Exception("Сумма двух сторон треугольника должна быть больше третьей");
+        }
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public string Type() {
+        if (a == b && b == c) {
+            type += "Равносторонний, ";
+        } else if (a == b || a == c || b == c) {
+            type += "Равнобедренный, ";
+        } else {
+            type += "Разносторонний, ";
+        }
+
+        if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a) {
+            type += "Прямоугольный";
+        } else if (a * a + b * b < c * c || a * a + c * c < b * b || b * b + c * c < a * a) {
+            type += "Тупоугольный";
+        } else {
+            type += "Остроугольный";
+        }
+
+        return type;
+    }
+
+    public double Area() {
+        double p = (a + b + c) / 2;
+        return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    public int Perimeter() {
+        return a + b + c;
+    }
+}
+
+public class Code {
+    public static void Main() {
+        try {
+            Console.Write("Введите длину стороны a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите длину стороны b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите длину стороны c: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Triangle triangle = new Triangle(a, b, c);
+
+            Console.WriteLine($"Тип треугольника: {triangle.Type()}");
+            Console.WriteLine($"Площадь треугольника: {triangle.Area()}");
+            Console.WriteLine($"Периметр треугольника: {triangle.Perimeter()}");
+        } catch (Exception ex) {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        
+    }
+}

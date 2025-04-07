05415fa1730af9cede6fc4c8ae71c6ae560a75c1
rotat1ck <rotatick32151@gmail.com>
2025-04-07 11:56:28 +0300

    07.04 classwork

---
+++ classwork/2025.04/unittest/Program.cs
@@ -0,0 +1,9 @@
+﻿class Code {
+    static void Main() {
+        BankAccount ba = new("Vova", 2000);
+
+        ba.Credit(200.44);
+        ba.Debit(123.33);
+        Console.WriteLine($"Balance: {ba.Balance}");
+    }
+}

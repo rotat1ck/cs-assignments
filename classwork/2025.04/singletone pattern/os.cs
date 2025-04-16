7f48f65f36f3ced444449068e9d76d8c384dae2a
rotat1ck <rotatick32151@gmail.com>
2025-04-16 11:32:52 +0300

    16.04 classwork

---
+++ classwork/2025.04/singletone pattern/os.cs
@@ -0,0 +1,15 @@
+class OS {
+    private static OS? instance;
+
+    public string Name { get; private set; }
+    protected OS(string name) {
+        this.Name = name;
+    }
+
+    public static OS getInstance(string name) {
+        if (instance == null) {
+            instance = new OS(name);
+        }
+        return instance;
+    }
+}

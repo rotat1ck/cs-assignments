c6a951ff81ad4560d4c45dbd1d9e18bcc893d261
rotat1ck <182702594+rotat1ck@users.noreply.github.com>
2025-05-26 13:31:36 +0300

    First client-side iteration test

---
+++ final project/client/MainWindow.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace client {
+    partial class MainWindow {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            TestLabel = new Label();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // TestLabel
+            // 
+            TestLabel.AutoSize = true;
+            TestLabel.Location = new Point(12, 9);
+            TestLabel.Name = "TestLabel";
+            TestLabel.Size = new Size(71, 20);
+            TestLabel.TabIndex = 0;
+            TestLabel.Text = "TestLabel";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 32);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(682, 406);
+            dataGridView1.TabIndex = 2;
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(TestLabel);
+            Name = "MainWindow";
+            Text = "MainWindow";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label TestLabel;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ final project/client/MainWindow.cs
@@ -0,0 +1,20 @@
+﻿using SqliteDB;
+
+namespace client {
+    public partial class MainWindow : Form {
+        private DBController controller = new DBController("client.db");
+        public MainWindow() {
+            InitializeComponent();
+
+            controller.objectsModel.CreateRecord(new Objects {
+                object_type = 1,
+                name = "Goida House",
+                description = "Goida Description",
+                location = "Goida Street",
+                number = 1
+            });
+            List<Objects> objects = controller.objectsModel.Query();
+            dataGridView1.DataSource = objects;
+        }
+    }
+}

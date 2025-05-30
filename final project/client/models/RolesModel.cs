ae41aadf74e19e3c1330b78dcace662bf2378b22
rotat1ck <182702594+rotat1ck@users.noreply.github.com>
2025-05-30 10:26:17 +0300

    Added new data table models

---
+++ final project/client/models/RolesModel.cs
@@ -0,0 +1,12 @@
+﻿using SqliteDB;
+
+namespace client.models {
+    public class Roles : BaseModel<Roles> {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public Roles() : base() { }
+
+        public Roles(Database db) : base(db) { }
+    }
+}
---
+++ final project/client/models/TasksModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqliteDB;
+
+namespace client.models {
+    public class Tasks : BaseModel<Tasks> {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string content { get; set; }
+
+        public Tasks() : base() { }
+
+        public Tasks(Database db) : base(db) { }
+    }
+}

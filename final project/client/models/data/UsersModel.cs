0836e569480cc120a6b5459e02a07adcb37704c0
rotat1ck <rotatick32151@gmail.com>
2025-06-06 01:15:43 +0300

    [Dev branch merge] DB Sample  (#7)
    * Added rights column to users table
    
    * Added endstamp column to tasks_objects linking table
    
    * Models adaptation to new DB Sample

--- final project/client/models/data/UsersModel.cs
+++ final project/client/models/data/UsersModel.cs
@@ -6,6 +6,7 @@ namespace client.models.data {
 
         public int id { get; set; }
         public int employee_id { get; set; }
+        public int rights { get; set; }
         public string username { get; set; }
         public string password { get; set; }
         public string email { get; set; }

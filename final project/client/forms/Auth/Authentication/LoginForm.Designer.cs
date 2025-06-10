961188ffcbf285701061e767a7a2cbbba9eea68b
rotat1ck <182702594+rotat1ck@users.noreply.github.com>
2025-06-10 10:16:10 +0300

    Completed LoginForm

--- final project/client/forms/Auth/Authentication/LoginForm.Designer.cs
+++ final project/client/forms/Auth/Authentication/LoginForm.Designer.cs
@@ -25,7 +25,7 @@
         private void InitializeComponent() {
             SignInLabel = new Label();
             UsernameLabel = new Label();
-            textBox1 = new TextBox();
+            UsernameInput = new TextBox();
             label1 = new Label();
             PasswordInput = new TextBox();
             label2 = new Label();
@@ -36,9 +36,9 @@
             // SignInLabel
             // 
             SignInLabel.Font = new Font("Segoe UI", 16F);
-            SignInLabel.Location = new Point(10, 10);
+            SignInLabel.Location = new Point(11, 13);
             SignInLabel.Name = "SignInLabel";
-            SignInLabel.Size = new Size(210, 30);
+            SignInLabel.Size = new Size(240, 40);
             SignInLabel.TabIndex = 0;
             SignInLabel.Text = "Cherry: Sign In";
             SignInLabel.TextAlign = ContentAlignment.MiddleLeft;
@@ -46,45 +46,48 @@
             // UsernameLabel
             // 
             UsernameLabel.Font = new Font("Segoe UI", 10F);
-            UsernameLabel.Location = new Point(10, 80);
+            UsernameLabel.Location = new Point(11, 107);
             UsernameLabel.Name = "UsernameLabel";
-            UsernameLabel.Size = new Size(280, 30);
+            UsernameLabel.Size = new Size(320, 40);
             UsernameLabel.TabIndex = 1;
             UsernameLabel.Text = "Username or email:";
             UsernameLabel.TextAlign = ContentAlignment.MiddleLeft;
             // 
-            // textBox1
+            // UsernameInput
             // 
-            textBox1.Location = new Point(10, 110);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(280, 23);
-            textBox1.TabIndex = 2;
+            UsernameInput.Location = new Point(11, 147);
+            UsernameInput.Margin = new Padding(3, 4, 3, 4);
+            UsernameInput.Name = "UsernameInput";
+            UsernameInput.Size = new Size(319, 27);
+            UsernameInput.TabIndex = 2;
             // 
             // label1
             // 
             label1.Font = new Font("Segoe UI", 10F);
-            label1.Location = new Point(10, 160);
+            label1.Location = new Point(11, 213);
             label1.Name = "label1";
-            label1.Size = new Size(280, 30);
+            label1.Size = new Size(320, 40);
             label1.TabIndex = 3;
             label1.Text = "Password:";
             label1.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // PasswordInput
             // 
-            PasswordInput.Location = new Point(10, 190);
+            PasswordInput.Location = new Point(11, 253);
+            PasswordInput.Margin = new Padding(3, 4, 3, 4);
             PasswordInput.Name = "PasswordInput";
             PasswordInput.PasswordChar = '*';
-            PasswordInput.Size = new Size(280, 23);
+            PasswordInput.Size = new Size(319, 27);
             PasswordInput.TabIndex = 4;
             PasswordInput.UseSystemPasswordChar = true;
+            PasswordInput.KeyDown += PasswordInput_KeyDown;
             // 
             // label2
             // 
             label2.Font = new Font("Segoe UI", 10F);
-            label2.Location = new Point(10, 300);
+            label2.Location = new Point(11, 400);
             label2.Name = "label2";
-            label2.Size = new Size(160, 30);
+            label2.Size = new Size(196, 40);
             label2.TabIndex = 5;
             label2.Text = "Don't have an account?";
             label2.TextAlign = ContentAlignment.MiddleLeft;
@@ -92,9 +95,10 @@
             // RegisterButton
             // 
             RegisterButton.Font = new Font("Segoe UI", 10F);
-            RegisterButton.Location = new Point(180, 300);
+            RegisterButton.Location = new Point(206, 400);
+            RegisterButton.Margin = new Padding(3, 4, 3, 4);
             RegisterButton.Name = "RegisterButton";
-            RegisterButton.Size = new Size(110, 30);
+            RegisterButton.Size = new Size(126, 40);
             RegisterButton.TabIndex = 6;
             RegisterButton.Text = "Register";
             RegisterButton.UseVisualStyleBackColor = true;
@@ -102,26 +106,29 @@
             // SignInButton
             // 
             SignInButton.Font = new Font("Segoe UI", 20F);
-            SignInButton.Location = new Point(10, 340);
+            SignInButton.Location = new Point(11, 453);
+            SignInButton.Margin = new Padding(3, 4, 3, 4);
             SignInButton.Name = "SignInButton";
-            SignInButton.Size = new Size(280, 50);
+            SignInButton.Size = new Size(320, 67);
             SignInButton.TabIndex = 7;
             SignInButton.Text = "Sign In";
             SignInButton.UseVisualStyleBackColor = true;
+            SignInButton.Click += SignInButton_Click;
             // 
             // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(304, 401);
+            ClientSize = new Size(347, 535);
             Controls.Add(SignInButton);
             Controls.Add(RegisterButton);
             Controls.Add(label2);
             Controls.Add(PasswordInput);
             Controls.Add(label1);
-            Controls.Add(textBox1);
+            Controls.Add(UsernameInput);
             Controls.Add(UsernameLabel);
             Controls.Add(SignInLabel);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "LoginForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Sign In";
@@ -133,7 +140,7 @@
 
         private Label SignInLabel;
         private Label UsernameLabel;
-        private TextBox textBox1;
+        private TextBox UsernameInput;
         private Label label1;
         private TextBox PasswordInput;
         private Label label2;

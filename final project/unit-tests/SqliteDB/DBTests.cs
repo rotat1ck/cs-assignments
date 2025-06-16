6ea8fd803bf6c187ef06ae04e25161533834f996
rotat1ck <182702594+rotat1ck@users.noreply.github.com>
2025-06-16 16:20:46 +0300

    Some SqliteDB test to start

---
+++ final project/unit-tests/SqliteDB/DBTests.cs
@@ -0,0 +1,31 @@
+﻿using SqliteDB;
+using client;
+using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace unit_tests.SqliteDB {
+    [TestClass]
+    public sealed class DBTests {
+        [TestMethod]
+        public void DBInitTest() {
+            Database db = new Database("client.db");
+
+        }
+
+        [TestMethod]
+        public void DBDirectQueryTest() {
+            Database db = new Database("client.db");
+            SqliteCommand cmd = db.GetEmptyCommand();
+
+            cmd.CommandText = "SELECT * FROM users";
+            db.DirectQuery(cmd);
+        }
+
+        // OUTDATED
+        [TestMethod]
+        public void DBObjectQuery() {
+            Database db = new Database("client.db");
+            DataTable dt = db.ObjectQuery("SELECT username FROM users");
+        }   
+    }
+}
---
+++ final project/unit-tests/SqliteDB/ModelTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqliteDB;
+
+namespace unit_tests.SqliteDB {
+    public class Users : BaseModel<Users> {
+        //public new string _tablename = "users";
+
+        public int id { get; set; }
+        public int employee_id { get; set; }
+        public int rights { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+
+        public Users() : base() { }
+
+        public Users(Database db) : base(db) {
+
+        }
+    }
+
+    [TestClass]
+    public class ModelTests {
+        public static Database db = new Database("client.db");
+        public static Users usersModel = new Users(db);
+
+        [TestMethod]
+        public void Init() {
+            
+            
+        }
+
+        [TestMethod]
+        public void QueryAll() {
+            usersModel.Query();
+        }
+
+        [TestMethod]
+        public void Filter() {
+            usersModel.Filter(("username", "goida"));
+        }
+    }
+}

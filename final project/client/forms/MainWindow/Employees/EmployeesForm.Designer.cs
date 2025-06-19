ed838a797e87b61076fe8169f1c74723361ac5f3
rotat1ck <rotatick32151@gmail.com>
2025-06-19 05:06:37 +0300

    EmployeesForm design/logic update

--- final project/client/forms/MainWindow/Employees/EmployeesForm.Designer.cs
+++ final project/client/forms/MainWindow/Employees/EmployeesForm.Designer.cs
@@ -27,9 +27,22 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            collapsibleSidebar1 = new CollapsibleSidebar();
             label1 = new Label();
+            collapsibleSidebar1 = new CollapsibleSidebar();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button1 = new Button();
+            label2 = new Label();
+            panel1 = new Panel();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(57, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Список сотрудников";
             // 
             // collapsibleSidebar1
             // 
@@ -41,42 +54,82 @@
             collapsibleSidebar1.LayoutStyle = ToolStripLayoutStyle.VerticalStackWithOverflow;
             collapsibleSidebar1.Location = new Point(0, 0);
             collapsibleSidebar1.Name = "collapsibleSidebar1";
-            collapsibleSidebar1.Size = new Size(62, 453);
+            collapsibleSidebar1.Padding = new Padding(5, 2, 0, 2);
+            collapsibleSidebar1.Size = new Size(54, 346);
             collapsibleSidebar1.TabIndex = 0;
             collapsibleSidebar1.Text = "collapsibleSidebar1";
             // 
-            // label1
+            // tableLayoutPanel1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(326, 160);
-            label1.Name = "label1";
-            label1.Size = new Size(60, 20);
-            label1.TabIndex = 1;
-            label1.Text = "ЮЗЕРЫ";
+            tableLayoutPanel1.BackColor = Color.Transparent;
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Location = new Point(57, 27);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.Size = new Size(319, 307);
+            tableLayoutPanel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(443, 293);
+            button1.Name = "button1";
+            button1.Size = new Size(140, 41);
+            button1.TabIndex = 3;
+            button1.Text = "Добавить сотрудника";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(383, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(157, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Информация о сотруднике";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.Location = new Point(383, 27);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 260);
+            panel1.TabIndex = 5;
             // 
             // EmployeesForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.photo_2025_05_30_11_56_46;
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(682, 453);
-            Controls.Add(collapsibleSidebar1);
+            ClientSize = new Size(598, 346);
+            Controls.Add(panel1);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(tableLayoutPanel1);
             Controls.Add(label1);
+            Controls.Add(collapsibleSidebar1);
             MainMenuStrip = collapsibleSidebar1;
+            Margin = new Padding(3, 2, 3, 2);
             MaximizeBox = false;
-            MaximumSize = new Size(700, 500);
+            MaximumSize = new Size(614, 385);
             MinimizeBox = false;
-            MinimumSize = new Size(700, 500);
+            MinimumSize = new Size(614, 385);
             Name = "EmployeesForm";
-            Text = "EmployeesForm";
+            Text = "Управление сотрудниками";
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private CollapsibleSidebar collapsibleSidebar1;
         private Label label1;
+        private CollapsibleSidebar collapsibleSidebar1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button1;
+        private Label label2;
+        private Panel panel1;
     }
 }

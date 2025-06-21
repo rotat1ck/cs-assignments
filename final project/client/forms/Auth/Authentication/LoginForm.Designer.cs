005cf82b4aca5c8d8fe4c697783f4ff3f79ad1bd
rotat1ck <rotatick32151@gmail.com>
2025-06-21 08:32:45 +0300

    NewEmployee firtst design

--- final project/client/forms/Auth/Authentication/LoginForm.Designer.cs
+++ final project/client/forms/Auth/Authentication/LoginForm.Designer.cs
@@ -36,9 +36,9 @@ namespace client.forms.Auth.Authentication {
             // SignInLabel
             // 
             SignInLabel.Font = new Font("Segoe UI", 16F);
-            SignInLabel.Location = new Point(11, 13);
+            SignInLabel.Location = new Point(10, 10);
             SignInLabel.Name = "SignInLabel";
-            SignInLabel.Size = new Size(240, 40);
+            SignInLabel.Size = new Size(210, 30);
             SignInLabel.TabIndex = 0;
             SignInLabel.Text = "Cherry: Sign In";
             SignInLabel.TextAlign = ContentAlignment.MiddleLeft;
@@ -46,38 +46,36 @@ namespace client.forms.Auth.Authentication {
             // UsernameLabel
             // 
             UsernameLabel.Font = new Font("Segoe UI", 10F);
-            UsernameLabel.Location = new Point(11, 107);
+            UsernameLabel.Location = new Point(10, 80);
             UsernameLabel.Name = "UsernameLabel";
-            UsernameLabel.Size = new Size(320, 40);
+            UsernameLabel.Size = new Size(280, 30);
             UsernameLabel.TabIndex = 1;
             UsernameLabel.Text = "Username or email:";
             UsernameLabel.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // UsernameInput
             // 
-            UsernameInput.Location = new Point(11, 147);
-            UsernameInput.Margin = new Padding(3, 4, 3, 4);
+            UsernameInput.Location = new Point(10, 110);
             UsernameInput.Name = "UsernameInput";
-            UsernameInput.Size = new Size(319, 27);
+            UsernameInput.Size = new Size(280, 23);
             UsernameInput.TabIndex = 2;
             // 
             // label1
             // 
             label1.Font = new Font("Segoe UI", 10F);
-            label1.Location = new Point(11, 213);
+            label1.Location = new Point(10, 160);
             label1.Name = "label1";
-            label1.Size = new Size(320, 40);
+            label1.Size = new Size(280, 30);
             label1.TabIndex = 3;
             label1.Text = "Password:";
             label1.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // PasswordInput
             // 
-            PasswordInput.Location = new Point(11, 253);
-            PasswordInput.Margin = new Padding(3, 4, 3, 4);
+            PasswordInput.Location = new Point(10, 190);
             PasswordInput.Name = "PasswordInput";
             PasswordInput.PasswordChar = '*';
-            PasswordInput.Size = new Size(319, 27);
+            PasswordInput.Size = new Size(280, 23);
             PasswordInput.TabIndex = 4;
             PasswordInput.UseSystemPasswordChar = true;
             PasswordInput.KeyDown += PasswordInput_KeyDown;
@@ -85,9 +83,9 @@ namespace client.forms.Auth.Authentication {
             // label2
             // 
             label2.Font = new Font("Segoe UI", 10F);
-            label2.Location = new Point(11, 400);
+            label2.Location = new Point(10, 300);
             label2.Name = "label2";
-            label2.Size = new Size(196, 40);
+            label2.Size = new Size(172, 30);
             label2.TabIndex = 5;
             label2.Text = "Don't have an account?";
             label2.TextAlign = ContentAlignment.MiddleLeft;
@@ -95,10 +93,9 @@ namespace client.forms.Auth.Authentication {
             // RegisterButton
             // 
             RegisterButton.Font = new Font("Segoe UI", 10F);
-            RegisterButton.Location = new Point(206, 400);
-            RegisterButton.Margin = new Padding(3, 4, 3, 4);
+            RegisterButton.Location = new Point(180, 300);
             RegisterButton.Name = "RegisterButton";
-            RegisterButton.Size = new Size(126, 40);
+            RegisterButton.Size = new Size(110, 30);
             RegisterButton.TabIndex = 6;
             RegisterButton.Text = "Register";
             RegisterButton.UseVisualStyleBackColor = true;
@@ -106,10 +103,9 @@ namespace client.forms.Auth.Authentication {
             // SignInButton
             // 
             SignInButton.Font = new Font("Segoe UI", 20F);
-            SignInButton.Location = new Point(11, 453);
-            SignInButton.Margin = new Padding(3, 4, 3, 4);
+            SignInButton.Location = new Point(10, 340);
             SignInButton.Name = "SignInButton";
-            SignInButton.Size = new Size(320, 67);
+            SignInButton.Size = new Size(280, 50);
             SignInButton.TabIndex = 7;
             SignInButton.Text = "Sign In";
             SignInButton.UseVisualStyleBackColor = true;
@@ -117,9 +113,9 @@ namespace client.forms.Auth.Authentication {
             // 
             // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(347, 535);
+            ClientSize = new Size(304, 401);
             Controls.Add(SignInButton);
             Controls.Add(RegisterButton);
             Controls.Add(label2);
@@ -128,7 +124,8 @@ namespace client.forms.Auth.Authentication {
             Controls.Add(UsernameInput);
             Controls.Add(UsernameLabel);
             Controls.Add(SignInLabel);
-            Margin = new Padding(3, 4, 3, 4);
+            MaximumSize = new Size(320, 440);
+            MinimumSize = new Size(320, 440);
             Name = "LoginForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Sign In";
---
+++ final project/client/forms/Modals/NewEmployee/NewEmployeeForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace client.forms.Modals.NewEmployee {
+    partial class NewEmployeeForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            LastNameLabel = new Label();
+            LastNameInput = new TextBox();
+            FirstNameInput = new TextBox();
+            FirstNameLabel = new Label();
+            CreateLabel = new Label();
+            CancelButton = new Button();
+            CreateButton = new Button();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            comboBox2 = new ComboBox();
+            SuspendLayout();
+            // 
+            // LastNameLabel
+            // 
+            LastNameLabel.AutoSize = true;
+            LastNameLabel.ImeMode = ImeMode.NoControl;
+            LastNameLabel.Location = new Point(12, 90);
+            LastNameLabel.Name = "LastNameLabel";
+            LastNameLabel.Size = new Size(61, 15);
+            LastNameLabel.TabIndex = 31;
+            LastNameLabel.Text = "Фамилия:";
+            // 
+            // LastNameInput
+            // 
+            LastNameInput.Location = new Point(99, 87);
+            LastNameInput.Name = "LastNameInput";
+            LastNameInput.Size = new Size(223, 23);
+            LastNameInput.TabIndex = 30;
+            // 
+            // FirstNameInput
+            // 
+            FirstNameInput.Location = new Point(99, 43);
+            FirstNameInput.Name = "FirstNameInput";
+            FirstNameInput.Size = new Size(223, 23);
+            FirstNameInput.TabIndex = 29;
+            // 
+            // FirstNameLabel
+            // 
+            FirstNameLabel.AutoSize = true;
+            FirstNameLabel.ImeMode = ImeMode.NoControl;
+            FirstNameLabel.Location = new Point(12, 45);
+            FirstNameLabel.Name = "FirstNameLabel";
+            FirstNameLabel.Size = new Size(34, 15);
+            FirstNameLabel.TabIndex = 28;
+            FirstNameLabel.Text = "Имя:";
+            // 
+            // CreateLabel
+            // 
+            CreateLabel.AutoSize = true;
+            CreateLabel.ImeMode = ImeMode.NoControl;
+            CreateLabel.Location = new Point(95, 9);
+            CreateLabel.Name = "CreateLabel";
+            CreateLabel.Size = new Size(160, 15);
+            CreateLabel.TabIndex = 27;
+            CreateLabel.Text = "Введите данные сотрудника";
+            // 
+            // CancelButton
+            // 
+            CancelButton.ImeMode = ImeMode.NoControl;
+            CancelButton.Location = new Point(12, 238);
+            CancelButton.Name = "CancelButton";
+            CancelButton.Size = new Size(130, 35);
+            CancelButton.TabIndex = 26;
+            CancelButton.Text = "Отмена";
+            CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // CreateButton
+            // 
+            CreateButton.ImeMode = ImeMode.NoControl;
+            CreateButton.Location = new Point(192, 238);
+            CreateButton.Name = "CreateButton";
+            CreateButton.Size = new Size(130, 35);
+            CreateButton.TabIndex = 25;
+            CreateButton.Text = "Создать";
+            CreateButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 134);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 15);
+            label1.TabIndex = 32;
+            label1.Text = "Позиция:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(99, 131);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(223, 23);
+            comboBox1.TabIndex = 33;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 180);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 34;
+            label2.Text = "label2";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(99, 177);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(223, 23);
+            comboBox2.TabIndex = 35;
+            // 
+            // NewEmployeeForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(334, 285);
+            Controls.Add(comboBox2);
+            Controls.Add(label2);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Controls.Add(LastNameLabel);
+            Controls.Add(LastNameInput);
+            Controls.Add(FirstNameInput);
+            Controls.Add(FirstNameLabel);
+            Controls.Add(CreateLabel);
+            Controls.Add(CancelButton);
+            Controls.Add(CreateButton);
+            Name = "NewEmployeeForm";
+            Text = "Добавить сотрудника";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label LastNameLabel;
+        private TextBox LastNameInput;
+        private TextBox FirstNameInput;
+        private Label FirstNameLabel;
+        private Label CreateLabel;
+        private Button CancelButton;
+        private Button CreateButton;
+        private Label label1;
+        private ComboBox comboBox1;
+        private Label label2;
+        private ComboBox comboBox2;
+    }
+}

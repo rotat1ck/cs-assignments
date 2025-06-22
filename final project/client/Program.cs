700b03db7be3417e36f814c0a303c5452bcfe71f
rotat1ck <rotatick32151@gmail.com>
2025-06-22 11:18:51 +0300

    Early exit from Helper breaking program fix

--- final project/client/Program.cs
+++ final project/client/Program.cs
@@ -13,10 +13,16 @@ static class Program {
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
 
-        if (!File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "client.db"))) {
-            Application.Run(new EntryForm());
+        if (File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "client.db"))) {
+            DBController.InitDB(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "client.db"));
+            if (DBController.usersModel.Filter(("rights", "1")).Count > 0) {
+                Application.Run(new LoginForm());
+            } else {
+                MessageBox.Show("Фатальная ошибка. Требуется переустановка программы");
+                Environment.Exit(1);
+            }
         } else {
-            Application.Run(new LoginForm());
+            Application.Run(new EntryForm());
         }
     }    
 }

ae58612629f2738e142205f480ffafa97842ba42
rotat1ck <182702594+rotat1ck@users.noreply.github.com>
2025-10-27 12:57:50 +0300

    Http listener - 24.10

---
+++ classwork/2025.10/http-listener/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace http_listener {
+    internal class Program {
+        static async Task Main(string[] args) {
+            HttpListener serv = new();
+            serv.Start();
+            serv.Prefixes.Add("http://127.0.0.1:8888/connection/");
+
+            var context = await serv.GetContextAsync();
+            var request = context.Request;
+            var response = context.Response;
+            var user = context.User;
+
+            serv.Stop();
+        }
+    }
+}
